17f57a2d729410fa2c0ffa3b13ad168800931092
Vlad <vlad.chk@gmail.com>
2025-01-25 11:49:26 -0300

    Área de influencia NPC, nombres y índice de símbolos

--- Scenes/npc.gd
+++ Scenes/npc.gd
@@ -1,9 +1,14 @@
 extends CharacterBody2D
 
+class_name NPC
+
 var focused = false
 var hablando = false
-var tiene = ['a', 'b']
-var quiere = ['c']
+var cerca = false
+
+# Simbolos que tiene y quiere
+var tiene = []
+var quiere = []
 
 func _process(delta: float) -> void:
 	queue_redraw()
@@ -11,9 +16,11 @@ func _process(delta: float) -> void:
 func _ready():
 	$AnimatedSprite2D.play("idle")
 
+# Al ponerle el mouse encima, habla si el juagdor está cerca
 func _on_area_tooltip_mouse_entered() -> void:
 	focused = true
-	if not(hablando):
+	if cerca and not(hablando):
+		$Globito.desaparecer()
 		hablando = true
 		await $Tooltip.decir(tiene)
 		await $Tooltip.preguntar(quiere)
@@ -25,3 +32,15 @@ func _on_area_tooltip_mouse_exited() -> void:
 func _draw():
 	if focused:
 		draw_circle(Vector2.ZERO, 30, Color.WHITE, false)
+
+# Al entrar o salir del area de "Influencia", 
+# seteamos la variable `cerca`
+func _on_influencia_body_entered(body: Node2D) -> void:
+	if body is Player: 
+		cerca = true
+		$Globito.aparecer()
+		
+func _on_influencia_body_exited(body: Node2D) -> void:
+	if body is Player: 
+		cerca = false
+		$Globito.desaparecer()
--- Scripts/simbolos.gd
+++ Scripts/simbolos.gd
@@ -1,4 +1,72 @@
 extends Node
 
-var pj1 = ['a', 'b', 'c']
-var pj2 = ['d', 'b', 'e']
+var simbolos = {
+	 'agua' :  {
+		'imagen': preload('res://Assets/ICONOS/AGUA.png'),
+		'dominio': 'naturaleza',
+	},
+	 'escritura' :  {
+		'imagen': preload('res://Assets/ICONOS/ESCRITURA.png'),
+		'dominio': 'arte',
+	},
+	 'fabrica' :  {
+		'imagen': preload('res://Assets/ICONOS/FABRICA.png'),
+		'dominio': 'progreso',
+	},
+	 'foco' :  {
+		'imagen': preload('res://Assets/ICONOS/FOCO.png'),
+		'dominio': 'progreso',
+	},
+	 'fotografía' :  {
+		'imagen': preload('res://Assets/ICONOS/FOTOGRAFIA.png'),
+		'dominio': 'progreso',
+	},
+	 'gas' :  {
+		'imagen': preload('res://Assets/ICONOS/GAS.png'),
+		'dominio': 'progreso',
+	},
+	 'cultivo' :  {
+		'imagen': preload('res://Assets/ICONOS/MANOS.png'),
+		'dominio': 'naturaleza',
+	},
+	 'montañas' :  {
+		'imagen': preload('res://Assets/ICONOS/MONTAÑAS.png'),
+		'dominio': 'naturaleza',
+	},
+	 'pintura' :  {
+		'imagen': preload('res://Assets/ICONOS/PALETA.png'),
+		'dominio': 'arte',
+	},
+	 'plantita' :  {
+		'imagen': preload('res://Assets/ICONOS/PLANTITA.png'),
+		'dominio': 'naturaleza',
+	},
+	 'teatro' :  {
+		'imagen': preload('res://Assets/ICONOS/TEATRO.png'),
+		'dominio': 'arte',
+	},
+	 'torre' :  {
+		'imagen': preload('res://Assets/ICONOS/TORRE ENERGIA.png'),
+		'dominio': 'progreso',
+	},
+}
+
+# Recibe un par de NPCs y se devuelve los matches entre
+# el 'tiene' de uno y el 'quiere' del otro, o null si no hay
+func matchear(pj1: NPC, pj2: NPC):
+	var de_2_a_1 = []
+	for simbolo in pj1.quiere:
+		if simbolo in pj2.tiene:
+			de_2_a_1.append(pj1.quiere)
+	
+	var de_1_a_2 = []
+	for simbolo in pj2.quiere:
+		if simbolo in pj1.tiene:
+			de_1_a_2.append(pj1.quiere)
+			
+	if len(de_1_a_2) > 0 or len(de_2_a_1) > 0:
+		return [de_1_a_2, de_2_a_1]
+	else:
+		return null
+	
+	
