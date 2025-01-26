ef102760f04c6ce53c5105ad086ea21c0ea01425
Alejandra <comomaraleja@gmail.com>
2025-01-25 23:03:00 -0300

    modificaciones interacciones

--- Scripts/interacciones.gd
+++ Scripts/interacciones.gd
@@ -19,26 +19,39 @@ func interactuar(pj1: NPC, pj2: NPC):
 	var aborrece_1_y_tiene_2 = Utils.interseccion(pj1.aborrece, pj2.tiene)
 	var aborrece_2_y_tiene_1 = Utils.interseccion(pj2.aborrece, pj1.tiene)
 	
+	var aborrece_1_y_quiere_2 = Utils.interseccion(pj1.aborrece, pj2.quiere)
+	var aborrece_2_y_quiere_1 = Utils.interseccion(pj2.aborrece, pj1.quiere)
+	
 	var aborrecen_en_comun = Utils.interseccion(pj1.aborrece, pj2.aborrece)
 	var tienen_en_comun = Utils.interseccion(pj1.tiene, pj2.tiene)
 	var quieren_en_comun = Utils.interseccion(pj1.quiere, pj2.quiere)
+	
+	#Si uno quiere lo que el otro aborrece y viceversa, se alienan
+	if aborrece_1_y_quiere_2:
+		interacciones.append({'accion': 'alienar','quien': pj1, 'a_quien': pj2 })
+		
+	if aborrece_2_y_quiere_1:	
+		interacciones.append({'accion': 'alienar','quien': pj2, 'a_quien': pj1 })
 	
 	# Si ambos tienen algo que el otro aborrece
 	if aborrece_1_y_tiene_2 and aborrece_2_y_tiene_1:
 		# Espantoso, se trauman ambos
 		interacciones.append({ 'accion': 'aborrecer', 'quien': pj1, 'a_quien': pj2})
 		interacciones.append({ 'accion': 'aborrecer', 'quien': pj2, 'a_quien': pj1})
-	
-	# Si hay aborrecer unilateral 
-	elif aborrece_1_y_tiene_2:
-		if quiere_2_y_tiene_1:
-			# Se adquieren deseos
-			interacciones.append({ 'accion': 'asombrar', 'quien': pj2, 'a_quien': pj1})
 		
-	elif len(aborrece_2_y_tiene_1):
-		if quiere_1_y_tiene_2:
-			# Se adquieren deseos
-			interacciones.append({ 'accion': 'asombrar', 'quien': pj1, 'a_quien': pj2})
+	# Si hay aborrecer unilateral 
+	elif aborrece_1_y_tiene_2 or aborrece_2_y_tiene_1:
+		 
+		# Aun uno puede aprender del otro
+		if aborrece_1_y_tiene_2:
+			if quiere_2_y_tiene_1:
+				# Se adquieren lo que se desea y el otro tenga
+				interacciones.append({ 'accion': 'asombrar', 'quien': pj2, 'a_quien': pj1})
+			
+		if aborrece_2_y_tiene_1:
+			if quiere_1_y_tiene_2:
+				# Se adquieren lo que se desea y el otro tenga
+				interacciones.append({ 'accion': 'asombrar', 'quien': pj1, 'a_quien': pj2})
 		
 	# Ninguno se aborrece
 	else:
@@ -79,22 +92,30 @@ func despachar(interaccion):
 		sentenciar(de, a)
 	if interaccion['accion'] == 'inspirar':
 		inspirar(de, a)
+	if interaccion['accion']	 == 'alienar':
+		alienar(de, a)
 
-
+func alienar(pj1: NPC, pj2: NPC):
+	var opuestos = Utils.interseccion(pj1.quiere, pj2.aborrece)
+	var opuesto = Utils.pick_random(opuestos, 1)
+	pj1.quiere.erase(opuesto)
+	pj1.aborrece.append(opuesto)
+	pj2.quiere.erase(opuesto)
+	pj2.aborrece.append(opuesto)
+	
+	
 # aka Adquirir deseo. Le agrega a pj1.tiene todo lo de pj2.tiene que esté en pj1.quiere
 func asombrar(pj1: NPC, pj2: NPC):
 	var deseado = Utils.interseccion(pj1.quiere, pj2.tiene)
 	pj2.tiene.append_array(deseado)
 
-	
 # Le agrega a pj1.aborrece algo random no repetido de pj2.tiene
 func aborrecer(pj1: NPC, pj2: NPC):
 	var algo_de_2 = Utils.pick_random(pj2.tiene, 1)
 	while algo_de_2[0] in pj1.aborrece: 
 		algo_de_2 = Utils.pick_random(pj2.tiene, 1)
 	pj1.aborrece.append_array(algo_de_2)
-
-
+	
 # aka Adquirir ideas. Le agrega a pj1.tiene algo random no repetido de pj2.tiene
 func conversar(pj1: NPC, pj2: NPC):
 	
