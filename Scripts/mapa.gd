1711f49634ad5f7c1c58d6f4f22cd6fa3f3a012d
Vlad <vlad.chk@gmail.com>
2025-01-26 00:24:18 -0300

    Navegación

--- Scripts/mapa.gd
+++ Scripts/mapa.gd
@@ -3,7 +3,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	for marker in $Markers/Naturaleza.get_children():
+		Navegacion.points['naturaleza'].append(marker.position)
+	for marker in $Markers/Progreso.get_children():
+		Navegacion.points['progreso'].append(marker.position)
+	for marker in $Markers/Arte.get_children():
+		Navegacion.points['arte'].append(marker.position)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
